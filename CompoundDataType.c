ae22cfac07bd8699a547f820b54c5f109e979707
ammarm7 <ammarm7@gmail.com>
2016-10-18 17:07:38 -0400

    Added the begining of the CompundDataType example, I will need to come back to it in order to complete the concept of strings simply being an array of characters.

---
+++ CompoundDataType.c
@@ -0,0 +1,11 @@
+/* Compound data types allow us to store multiple values stored under each variable for example: an array. The values of a compund data types are all the same data type. But you can also have a compound data type, which has different data types inside it. Such as a structue. A structure is more or less similar to a "class" however a struct only contains info, it doesn't have fn's so there is no behaviour present in a struct.*
+
+
+
+
+
+
+
+#include <stdio.h>
+
+
